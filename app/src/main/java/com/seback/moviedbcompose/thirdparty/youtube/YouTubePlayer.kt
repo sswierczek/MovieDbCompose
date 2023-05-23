a105f1acbd583925f7e398df87da07a9fcc9effd
Sebastian Swierczek <sebastian.swierczek@paramount.com>
2023-05-23 18:15:02 +0200

    Add auto playback of trailers on rotate

--- app/src/main/java/com/seback/moviedbcompose/thirdparty/youtube/YouTubePlayer.kt
+++ app/src/main/java/com/seback/moviedbcompose/thirdparty/youtube/YouTubePlayer.kt
@@ -3,17 +3,18 @@ package com.seback.moviedbcompose.thirdparty.youtube
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
-
 @Composable
 fun YouTubePlayer(modifier: Modifier, videoId: String) {
-    val videoIdState by remember { mutableStateOf(videoId) }
+    val videoIdState by rememberSaveable { mutableStateOf(videoId) }
+    var playbackTime by rememberSaveable { mutableStateOf(0f) }
 
     AndroidView(
         modifier = modifier,
@@ -21,7 +22,16 @@ fun YouTubePlayer(modifier: Modifier, videoId: String) {
             YouTubePlayerView(context).apply {
                 addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
                     override fun onReady(youTubePlayer: YouTubePlayer) {
-                        youTubePlayer.cueVideo(videoIdState, 0f)
+                        if (playbackTime > 0) {
+                            youTubePlayer.loadVideo(videoIdState, playbackTime)
+                        } else {
+                            youTubePlayer.cueVideo(videoIdState, playbackTime)
+                        }
+                    }
+
+                    override fun onCurrentSecond(youTubePlayer: YouTubePlayer, second: Float) {
+                        super.onCurrentSecond(youTubePlayer, second)
+                        playbackTime = second
                     }
                 })
             }
